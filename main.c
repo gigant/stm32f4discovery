3fe0b01f097acc20727292e18c030950e613bdd9
gigant <muravev@gmail.com>
2012-04-24 09:33:12 +0600

    simple project. Consist STM drivers for periph. devices. Configured IAR project. Main.c make LED4 light.

---
+++ main.c
@@ -0,0 +1,45 @@
+ #include "stm32f4xx.h"
+
+int main(void)
+{
+    /* Structure for port init */
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  
+  /* Enable the GPIOD Clock on port D connected led */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* Configure the GPIOD */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  
+  GPIOD->BSRRH =GPIO_Pin_12; //off led4
+  GPIOD->BSRRL = GPIO_Pin_12; //on led4
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
